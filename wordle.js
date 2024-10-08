e8b0fab4de06d1552b39bdba17f17d28fbd68361
XxX <juanma_dud_94@hotmail.com>
2024-10-08 15:26:42 -0300

    Matriz de inputs agregada

---
+++ wordle.js
@@ -0,0 +1,11 @@
+const wordleMatrix = document.getElementById('wordle-container');
+
+for(let i = 0; i < 5; i++) {
+    for(let j = 0; j < 5; j++) {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.id = 'a'+i+j;
+
+        wordleMatrix.appendChild(input);
+    }
+}
