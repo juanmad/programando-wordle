aa826a8bbca21518d0358e14e6c0d03c1433b7cd
XxX <juanma_dud_94@hotmail.com>
2024-10-09 19:27:52 -0300

    Lista de palabras agregada y funcion para elegir aleatoriamente una palabra del array de wordsSplit

--- wordle.js
+++ wordle.js
@@ -35,3 +35,41 @@ keyboard.forEach((row, i) => {
 
     keyboardMatrix.appendChild(rowDiv);
 });
+
+const words = [
+    "mundo",
+    "cielo",
+    "fruta",
+    "plato",
+    "silla",
+    "perro",
+    "nubes",
+    "piano",
+    "corso",
+    "fuego",
+    "salas",
+    "lomos",
+    "canto",
+    "viento",
+    "pacto",
+    "hojas",
+    "barco",
+    "jugar",
+    "pesca",
+    "cuero",
+    "suelo",
+    "olivo",
+    "carne",
+    "hueso",
+    "cable",
+    "pieza",
+  ];
+  
+const wordsSplit = words.map(word => word.split(''));
+
+const secretWord = () => {
+    const randomIndex = Math.floor(Math.random() * wordsSplit.length);
+    return wordsSplit[randomIndex];
+  };
+
+console.log(secretWord());
