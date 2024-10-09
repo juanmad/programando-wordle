580f58bd31978930571e7519a3d957908589be6a
XxX <juanma_dud_94@hotmail.com>
2024-10-09 20:48:21 -0300

    Evento del teclado backspace para borrar la palabra en la fila actual y mover el focus al previo input y también mover el focus al siguiente al escribir una letra

--- assets/scripts/wordle.js
+++ assets/scripts/wordle.js
@@ -1,5 +1,18 @@
-export const createWordleBoard = () => {
+const moveToNextInput = (i, j) => {
+    const nextInput = document.getElementById(`a${i}${j + 1}`);
+    if (nextInput) { // Si existe mover el focus al siguiente input
+        nextInput.focus(); 
+    }
+};
 
+const moveToPreviousInput = (i, j) => {
+    const prevInput = document.getElementById(`a${i}${j - 1}`);
+    if (prevInput) { // Si existe mover el focus al previo input
+        prevInput.focus();
+    }
+};
+
+export const createWordleBoard = () => {
     const wordleMatrix = document.getElementById('wordle-container');
 
     for (let i = 0; i < 5; i++) {
@@ -11,8 +24,23 @@ export const createWordleBoard = () => {
             input.autocomplete = 'off';
             input.setAttribute('maxlength', '1');
 
+            input.addEventListener('input', (event) => {
+                const character = event.target.character;
+                if (character.length === 1) {
+                    moveToNextInput(i, j);
+                }
+            });
+
+            // Evento keydown para tecla Backspace o Delete
+            input.addEventListener('keydown', (event) => {
+                if (event.key === 'Backspace') {
+                    event.preventDefault();
+                    input.value = '';
+                    moveToPreviousInput(i, j);
+                }
+            });
+
             wordleMatrix.appendChild(input);
         }
     }
-
-}
+};
