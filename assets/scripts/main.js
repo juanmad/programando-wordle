b75bac6639c16377ba8bc8b6c0c06222f2e54867
XxX <juanma_dud_94@hotmail.com>
2024-10-12 01:09:38 -0300

    Modificado words.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,13 +2,24 @@ import { createWordleBoard } from './board.js';
 import { createKeyboard } from './keyboard.js';
 import { getWord } from './keyboard.js';
 import { randomSecretWord } from './words.js';
+import { enableInput } from './gamelogic.js';
 
 
 // Initialize the game
 createWordleBoard();
 createKeyboard();
-const secretWord = randomSecretWord();
-console.log(secretWord);
 
 // para probar en el inspector de manera global hay que asignar a window para que esté disponible
 window.getWord = getWord;
+enableInput();
+
+
+import { wordsArray } from './words.js';
+import { wordsArraySorted } from './words.js';
+
+window.wordsArray = wordsArray;
+
+window.wordsArraySorted = wordsArraySorted;
+
+console.log('wordsArray:', wordsArray);
+console.log('wordsArraySorted:', wordsArraySorted);
--- assets/scripts/words.js
+++ assets/scripts/words.js
@@ -1,34 +1,45 @@
-const words = [
-    "mundo",
-    "cielo",
-    "fruta",
-    "plato",
-    "silla",
-    "perro",
-    "nubes",
-    "piano",
-    "corso",
-    "fuego",
-    "salas",
-    "lomos",
-    "canto",
-    "pacto",
-    "hojas",
-    "barco",
-    "jugar",
-    "pesca",
-    "cuero",
-    "suelo",
-    "olivo",
-    "carne",
-    "hueso",
-    "cable",
-    "pieza",
-];
+// const words = [
+//     "mundo",
+//     "cielo",
+//     "fruta",
+//     "plato",
+//     "silla",
+//     "perro",
+//     "nubes",
+//     "piano",
+//     "corso",
+//     "fuego",
+//     "salas",
+//     "lomos",
+//     "canto",
+//     "pacto",
+//     "hojas",
+//     "barco",
+//     "jugar",
+//     "pesca",
+//     "cuero",
+//     "suelo",
+//     "olivo",
+//     "carne",
+//     "hueso",
+//     "cable",
+//     "pieza",
+//     "suero",
+// ];
 
-const wordsSplit = words.map(word => word.split(''));
+export const words = [
+    "suelo",
+    "cuero",
+    "ruido",
+    "eucro",
+    "aaaaa",
+    // "mundo",
+]
+
+// Array de array de caracteres
+export const wordsArray = words.map(word => word.split(''));
 
 export const randomSecretWord = () => {
-    const randomIndex = Math.floor(Math.random() * wordsSplit.length);
-    return wordsSplit[randomIndex];
+    const randomIndex = Math.floor(Math.random() * wordsArray.length);
+    return wordsArray[randomIndex];
 };
