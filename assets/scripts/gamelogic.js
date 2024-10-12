e292cfe6edfe862c5527e060d4633596a4123262
XxX <juanma_dud_94@hotmail.com>
2024-10-12 01:06:34 -0300

    Agregar gamelogic

---
+++ assets/scripts/gamelogic.js
@@ -0,0 +1,82 @@
+import { getWord } from './keyboard.js';
+import { wordsArray } from './words.js';
+import { wordsArraySorted } from './words.js';
+import { words, randomSecretWord } from './words.js';
+
+import { boardRows, wordLength, activeRow } from './board.js';
+
+const secretWord = randomSecretWord();
+console.log('secretWord:', secretWord);
+
+
+export const enableInput = () => {
+    for (let i = 0; i < wordLength; i++) {
+        const input = document.getElementById(`input-${activeRow}-${i}`);
+        input.disabled = false;
+    }
+}
+
+export const disableInput = () => {
+    for (let i = 0; i < wordLength; i++) {
+        const input = document.getElementById(`input-${activeRow}-${i}`);
+        input.disabled = true;
+    }
+}
+
+export const checkWordinArray = () => {
+    const word = getWord(`board-row-${activeRow}`);
+    console.log('Word:', typeof word, word);
+    if (words.includes(word)) {
+        console.log('word in array');
+        return true;
+    } else {
+        console.log('word not in array');
+        return false;
+    }
+}
+
+export const wordMatches = () => {
+    let match = 0;
+    const word = getWord(`board-row-${activeRow}`).split('');
+    console.log('wordMatches:', word);
+    
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].toLowerCase() !== secretWord[i].toLowerCase()) {
+            match++;
+        }
+    }
+    
+    console.log('match:', match);
+    return match === 0;
+}
+
+export const checkWordMatch = () => {
+    const word = getWord(`board-row-${activeRow}`).split('');
+    console.log('checkWordMatch:', word);
+
+    if (wordMatches()) {
+        console.log('word matches');
+        for (let i = 0; i < wordLength; i++) {
+            const input = document.getElementById(`input-${activeRow}-${i}`);
+            input.style.backgroundColor = '#538d4e';
+            input.style.borderColor = '#538d4e';
+        }
+        return;
+    }
+    console.log('word does not match');
+
+    word.forEach((letter, i) => {
+        const input = document.getElementById(`input-${activeRow}-${i}`);
+        if (letter.toLowerCase() === secretWord[i].toLowerCase()) {
+            console.log("Y", letter, 'pos:', i);
+            input.style.backgroundColor = '#538d4e';
+        } else if (secretWord.includes(letter)){
+            console.log("N", letter, 'pos:', i);
+            input.style.backgroundColor = '#b59f3b';
+            input.style.borderColor = '#b59f3b';
+        } else {
+            input.style.backgroundColor = '#3a3a3c';
+            input.style.borderColor = '#3a3a3c';
+        }
+    });
+}
