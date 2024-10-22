ef93366fb82774ae725237a7296a355b825add90
XxX <juanma_dud_94@hotmail.com>
2024-10-22 00:10:17 -0300

    Lógica del juego arreglada para corregir los bugs que habían

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -1,11 +1,9 @@
 import { getWord, clickedKey } from './keyboard.js';
 import { words, randomSecretWord } from './words.js';
-
 import { boardRows, wordLength, nextActiveRow, activeRow } from './board.js';
 
 const secretWord = randomSecretWord();
 console.log('secretWord:', secretWord);
-
 
 export const enableInput = () => {
     for (let i = 0; i < wordLength; i++) {
@@ -34,58 +32,52 @@ export const checkWordinArray = () => {
 }
 
 export const wordMatches = () => {
-    let match = 0;
     const word = getWord(`board-row-${activeRow}`).split('');
-    console.log('wordMatches:', word);
-
-    for (let i = 0; i < word.length; i++) {
-        if (word[i].toLowerCase() !== secretWord[i].toLowerCase()) {
-            match++;
-        }
-    }
-
-    console.log('match:', match);
-    return match === 0;
+    return word.every((letter, i) => letter.toLowerCase() === secretWord[i].toLowerCase());
 }
 
 export const checkWordMatch = () => {
     const word = getWord(`board-row-${activeRow}`).split('');
     console.log('checkWordMatch:', word);
 
+    const letterCount = {};
+    secretWord.forEach(letter => {
+        letterCount[letter] = (letterCount[letter] || 0) + 1;
+    });
+
     if (wordMatches()) {
         console.log('word matches');
-        for (let i = 0; i < wordLength; i++) {
+        word.forEach((_, i) => {
             const input = document.getElementById(`input-${activeRow}-${i}`);
             input.style.backgroundColor = '#538d4e';
             input.style.borderColor = '#538d4e';
-        }
+        });
     } else {
         console.log('word does not match');
 
         word.forEach((letter, i) => {
             const input = document.getElementById(`input-${activeRow}-${i}`);
+            const key = document.getElementById(`key-${letter.toUpperCase()}`);
+
             if (letter.toLowerCase() === secretWord[i].toLowerCase()) {
-                console.log("Y", letter, 'pos:', i);
                 input.style.backgroundColor = '#538d4e'; // Verde
                 input.style.borderColor = '#538d4e';
-
-                // BUG: si es que hay más dos letras iguales en getWord igual se pone en amarillo una de las letras en la posicion que no
-                // correcta, para reproducir secretWord = "suelo" y getWord = "lomos"
-            } else if (secretWord.includes(letter)) {
-                console.log("N", letter, 'pos:', i);
+                letterCount[letter]--; // Decrementar count para la letra encontrada
+            } else if (secretWord.includes(letter) && letterCount[letter] > 0) {
                 input.style.backgroundColor = '#b59f3b'; // Amarillo
                 input.style.borderColor = '#b59f3b';
+                key.style.backgroundColor = '#b59f3b';
+                letterCount[letter]--; // Decrementar count para la letra encontrada
             } else {
                 input.style.backgroundColor = '#3a3a3c'; // Gris
                 input.style.borderColor = '#3a3a3c';
-                
-                const key = document.getElementById(`key-${letter.toUpperCase()}`);
                 key.style.backgroundColor = '#303030'; // Gris oscuro
                 key.classList.add('disabled'); // Agregar clase disabled
-                key.removeEventListener('click', clickedKey); // Quitar el event listener para desactivar el div    
+                key.removeEventListener('click', clickedKey); // Quitar el event listener
             }
         });
     }
+    
     disableInput();
     nextActiveRow();
     enableInput();
